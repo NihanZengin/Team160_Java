5602d3d909854999e0d365377930e2a2dc946616
NihanZengin <nihan.zengin90@gmail.com>
2025-01-15 12:32:10 +0100

    ExplicitDataCasting

--- src/D03_dataCasting/C01_DataCasting_AutoWidening.java
+++ src/D03_dataCasting/C01_DataCasting_AutoWidening.java
@@ -86,52 +86,7 @@ public class C01_DataCasting_AutoWidening {
         dbl = lng;
         dbl = flt;
 
-        //Sayi degeri alan byte, short. int, long, float ve double variable'lar icin;
-        //Bir variable'a kendisinden daha genis data turundeki bir deger ATANABILIR MI|?
-        //Bu islemi Java otomatik olarak yapmaz.
-        //Java sorumlulugu sizin almanizi ister.
-        //Esitligin sagina parantez icinde cevirmek istediginiz data turunu yazariz.
-        //Buna Explicit Narrowing (Zorlayarak daraltma) denir.
 
-        byt = (byte) shrt; // 23
-        byt = (byte) sayiInt; // 30
-        byt = (byte) lng; //40
-        byt = (byte)flt; // 3.5F
-        byt = (byte) dbl; // 4.5
-
-
-        shrt = byt;
-        shrt = (short) sayiInt;
-        shrt = (short)lng;
-        shrt = (short)flt;
-        shrt =(short) dbl;
-
-        sayiInt = byt;
-        sayiInt = shrt;
-        sayiInt =(int) lng;
-        sayiInt = (int)flt;
-        sayiInt = (int)dbl;
-
-        lng = byt;
-        lng = shrt;
-        lng = sayiInt;
-        lng = (long)flt;
-        lng = (long) dbl;
-
-        flt = byt;
-        flt = shrt;
-        flt = sayiInt;
-        flt = lng;
-        flt =(float) dbl;
-
-        dbl = byt;
-        dbl = shrt;
-        dbl = sayiInt;
-        dbl = lng;
-        dbl = flt;
-
-        //boolean veya String data turundeki bir variable'a
-        //baska data turundeki degerleri ATAYAMAYIZ.
 
 
 
