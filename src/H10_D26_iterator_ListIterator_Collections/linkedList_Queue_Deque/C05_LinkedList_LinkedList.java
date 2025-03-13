0f386bc8c431bf815a36d8f26268280c3de10511
NihanZengin <nihan.zengin90@gmail.com>
2025-03-13 16:19:04 +0100

    Collections

---
+++ src/H10_D26_iterator_ListIterator_Collections/linkedList_Queue_Deque/C05_LinkedList_LinkedList.java
@@ -0,0 +1,19 @@
+package H10_D26_iterator_ListIterator_Collections.linkedList_Queue_Deque;
+
+import java.util.LinkedList;
+
+public class C05_LinkedList_LinkedList {
+
+    public static void main(String[] args) {
+
+        /*
+            LinkedList List, Queue ve Deque interface'lerini implement etmistir
+            dolayisiyla o 3 interface'deki tum ozelliklere sahiptir
+         */
+
+        LinkedList<Integer> sayilar = new LinkedList<>();
+
+
+
+    }
+}
